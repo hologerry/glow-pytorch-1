cb926a94c4e262ae7d698aa8fc1fe88d8ed3bab9
Gerry Gao <hologerry@gmail.com>
2019-10-06 20:29:25 +0800

    fix bug

--- data/explo.py
+++ data/explo.py
@@ -5,7 +5,7 @@ from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
 
-from .base import find_images_and_annotation
+from .base import make_dataset_base_image_attr
 
 
 class ExploDataset(Dataset):
@@ -20,7 +20,7 @@ class ExploDataset(Dataset):
         self.base_dir = os.path.join(self.root_dir, 'base')
         assert os.path.exists(self.base_dir), f"explo dataset {self.base_dir} does not exist"
 
-        self.datas = find_images_and_annotation(self.root_dir, "attributes.txt")
+        self.datas = make_dataset_base_image_attr(self.base_dir, self.root_dir, "attributes.txt")
         self.dataset_size = len(self.datas)
         self.train_size = 52 * 120  # train_char * train_font
         self.train_set = self.datas[:self.train_size]

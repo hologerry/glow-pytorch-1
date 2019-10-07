9d3f95f4ff7f0a1361796a9b2554e3c229aad9b7
Gerry Gao <hologerry@gmail.com>
2019-10-07 12:42:01 +0800

    fix sample bug

--- train.py
+++ train.py
@@ -32,8 +32,8 @@ parser.add_argument('--warm', default=False, type=bool, help="whether or not war
 parser.add_argument('--img_size', default=64, type=int, help='image size')
 parser.add_argument('--temp', default=0.7, type=float, help='temperature of sampling')
 parser.add_argument('--n_sample', default=32, type=int, help='number of samples')
-parser.add_argument('--sample_freq', default=500, type=int, help='interval of sample and reverse')
-parser.add_argument('--check_freq', default=2000, type=int, help='interval of save checkpoints')
+parser.add_argument('--sample_freq', default=100, type=int, help='interval of sample and reverse')
+parser.add_argument('--check_freq', default=1000, type=int, help='interval of save checkpoints')
 parser.add_argument('--experiment_dir', default='experiment', type=str,
                     help="experiments directory save the samples and checkpoint")
 parser.add_argument('path', metavar='PATH', type=str, help='Path to image(dataset) directory')
@@ -221,7 +221,7 @@ def train(args, model, encoder, optimizer, z_criterion):
 
                     # sample at first
                     utils.save_image(
-                        model_single.reverse(z_sample_first, z_sample_first)[1].cpu().data,
+                        model_single.reverse(z_sample_first)[1].cpu().data,
                         os.path.join(log_dir, 'sample', f'{str(i).zfill(6)}_first.png'),
                         normalize=True,
                         nrow=args.n_sample//4,
@@ -229,7 +229,7 @@ def train(args, model, encoder, optimizer, z_criterion):
                     )
                     # sample
                     utils.save_image(
-                        model_single.reverse(z_sample, z_sample)[1].cpu().data,
+                        model_single.reverse(z_sample)[1].cpu().data,
                         os.path.join(log_dir, 'sample', f'{str(i).zfill(6)}_sample.png'),
                         normalize=True,
                         nrow=args.n_sample//4,
@@ -237,7 +237,7 @@ def train(args, model, encoder, optimizer, z_criterion):
                     )
                     # reverse
                     utils.save_image(
-                        model_single.reverse(z_sample, z_sample, reconstruct=True)[1].cpu().data,
+                        model_single.reverse(z_sample, reconstruct=True)[1].cpu().data,
                         os.path.join(log_dir, 'sample', f'{str(i).zfill(6)}_sample_reconstruct.png'),
                         normalize=True,
                         nrow=args.batch//4,
@@ -245,8 +245,8 @@ def train(args, model, encoder, optimizer, z_criterion):
                     )
                     # reconstruct
                     utils.save_image(
-                        model_single.reverse(z_encode_c, z_encode_t, reconstruct=True)[1].cpu().data,
-                        os.path.join(log_dir, 'sample', f'{str(i).zfill(6)}_reconstruct.png'),
+                        model_single.reverse(z_encode_t, reconstruct=True)[1].cpu().data,
+                        os.path.join(log_dir, 'sample', f'{str(i).zfill(6)}_target_reconstruct.png'),
                         normalize=True,
                         nrow=args.batch//4,
                         range=(-0.5, 0.5),
@@ -254,7 +254,7 @@ def train(args, model, encoder, optimizer, z_criterion):
                     # ground truth
                     utils.save_image(
                         image.cpu().data,
-                        os.path.join(log_dir, 'sample', f'{str(i).zfill(6)}_ground-truth.png'),
+                        os.path.join(log_dir, 'sample', f'{str(i).zfill(6)}_target_ground-truth.png'),
                         normalize=True,
                         nrow=args.batch//4,
                         range=(-0.5, 0.5),
@@ -267,6 +267,12 @@ def train(args, model, encoder, optimizer, z_criterion):
                 )
                 torch.save(
                     model.state_dict(), os.path.join(log_dir, 'checkpoint', 'model_latest.pt')
+                )
+                torch.save(
+                    encoder.state_dict(), os.path.join(log_dir, 'checkpoint', f'encoder_{str(i).zfill(6)}.pt')
+                )
+                torch.save(
+                    encoder.state_dict(), os.path.join(log_dir, 'checkpoint', 'encoder_latest.pt')
                 )
                 torch.save(
                     optimizer.state_dict(), os.path.join(log_dir, 'checkpoint', f'optimizer_{str(i).zfill(6)}.pt')
